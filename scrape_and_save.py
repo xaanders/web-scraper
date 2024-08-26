7b269c00577e228fff122783959bfb4dce1c002f
Oleksandr Riazantsev <riazantsev.aleks@gmail.com>
2024-08-26 10:37:00 -0400

    script  prettifying

--- scrape_and_save.py
+++ scrape_and_save.py
@@ -10,29 +10,29 @@ import re
 # Create a global connection object
 connection = None
 counter = 0
-no_access_items = ['cloudflare', 'wordpress']
+no_access_items = ["cloudflare", "wordpress"]
+
 
 def get_db_connection():
     global connection
     try:
         if connection is None or not connection.is_connected():
             connection = mysql.connector.connect(
-                host="10.0.0.74", user="root1", password="123456", database="test"
+                host="localhost", user="root", password="123456", database="test"
             )
             print("NEW CONNECTION ESTABLISHED")
-        
+
         return connection
 
     except Exception as e:
         print(f"Couldn't connect to the database: {e}")
-        
 
 
 def get_items_to_scrape():
     connection = get_db_connection()
     cursor = connection.cursor(dictionary=True)
     query = """
-    SELECT company_id, user_id, email, link, status, last_scrape_date, next_scrape_date, 
+    SELECT company_id, user_id, email, url, status, last_scrape_date, next_scrape_date, 
            initial_number, current_number, new_items, keywords, status
     FROM track_company WHERE status = 1
     LIMIT 100;
@@ -48,7 +48,7 @@ def update_item(item):
     cursor = connection.cursor()
     query = """
     UPDATE track_company
-    SET current_number = %s, new_items = %s, next_scrape_date = %s, last_scrape_date = %s, status = %s, company_name = %s
+    SET current_number = %s, new_items = %s, next_scrape_date = %s, last_scrape_date = %s, status = %s, company_name = %s, matched_keywords = %s
     WHERE company_id = %s;
     """
     cursor.execute(
@@ -60,33 +60,13 @@ def update_item(item):
             item["last_scrape_date"],
             item["status"],
             item["company_name"],
-            item["company_id"]
+            item["matched_keywords"],
+            item["company_id"],
         ),
     )
     connection.commit()
     cursor.close()
 
-
-def scrape_website_soap(url, keywords):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    text = soup.get_text().lower()
-    company_name = url.split('.')[1].replace('/', '-')
-
-    with open( company_name + '.txt', "w", encoding="utf-8") as f:
-        f.write(text)
-
-    count = 0
-    for keyword in keywords:
-        count += text.count(keyword.lower().strip())
-        global no_access_items
-
-        status = 1
-        for d_item in no_access_items:
-            if text.count(d_item.lower().strip()):
-                status = 0;
-    
-    return [count, company_name, status]
 
 def store_user_items_for_notification(user_items):
     connection = get_db_connection()
@@ -104,7 +84,6 @@ def store_user_items_for_notification(user_items):
     cursor.close()
 
 
-
 async def scrape_and_find_keywords(page, url, keywords):
     matching_keywords = []
     try:
@@ -112,17 +91,16 @@ async def scrape_and_find_keywords(page, url, keywords):
         await page.goto(url)
 
         # Wait for network idle state to ensure page has loaded completely
-        await page.wait_for_load_state('networkidle')
+        await page.wait_for_load_state("networkidle")
 
         # Get the full page text content
         text_content = await page.content()
 
         # Optional: Save the content to a file for debugging purposes
-        safe_filename = url.split("//")[-1].split('/')[0].replace('.', '_') + '.txt'
+        safe_filename = url.split("//")[-1].split("/")[0].replace(".", "_") + ".txt"
         with open(safe_filename, "w", encoding="utf-8") as f:
             f.write(text_content)
 
-        
         for na_item in no_access_items:
             if na_item in text_content:
                 return [na_item]
@@ -136,7 +114,6 @@ async def scrape_and_find_keywords(page, url, keywords):
             if matches:
                 # Append the keyword to the matching_keywords list for each match found
                 matching_keywords.extend([keyword] * len(matches))
-
 
     except Exception as e:
         print(f"Error scraping {url}: {e}")
@@ -156,10 +133,12 @@ async def scrape_website_playwright(items):
             for item in items:
                 try:
                     page = await browser.new_page()
-                    task = scrape_and_find_keywords(page, item["link"], item["keywords"].split(","))
+                    task = scrape_and_find_keywords(
+                        page, item["url"], item["keywords"].split(",")
+                    )
                     tasks.append(task)
                 except Exception as e:
-                    print(f"Error creating page for {item['link']}: {e}")
+                    print(f"Error creating page for {item['url']}: {e}")
 
             # Gather results from all tasks concurrently
             results = await asyncio.gather(*tasks, return_exceptions=True)
@@ -167,28 +146,29 @@ async def scrape_website_playwright(items):
             # Process the results and update the item data
             for item, result in zip(items, results):
                 if isinstance(result, Exception):
-                    print(f"Error processing {item['link']}: {result}")
+                    print(f"Error processing {item['url']}: {result}")
                     item["current_number"] = 0
-                    item["matched_items"] = []
+                    item["matched_keywords"] = ""
                 else:
                     item["current_number"] = len(result)
-                    item["matched_items"] = result
-                    item["company_name"] = re.sub(r'[^a-zA-Z]', '', item["link"].split('.')[1])
+                    item["matched_keywords"] = ",".join(result)
+                    item["company_name"] = re.sub(
+                        r"[^a-zA-Z]", "", item["url"].split(".")[1]
+                    )
                     new_items.append(item)
                     for na_item in no_access_items:
                         if na_item in result:
                             item["status"] = 3
 
-
             # for item, result in zip(items, results):
             #     if isinstance(result, Exception):
-            #         print(f"Error processing {item['link']}: {result}")
+            #         print(f"Error processing {item['url']}: {result}")
             #         item["current_number"] = 0
-            #         item["matched_items"] = []
+            #         item["matched_keywords"] = []
             #     else:
             #         item["current_number"] = len(result)
-            #         item["matched_items"] = result
-            #         item["company_name"] = re.sub(r'[^a-zA-Z]', '', item["link"].split('.')[1])
+            #         item["matched_keywords"] = result
+            #         item["company_name"] = re.sub(r'[^a-zA-Z]', '', item["url"].split('.')[1])
 
             # Close the browser instance
             await browser.close()
